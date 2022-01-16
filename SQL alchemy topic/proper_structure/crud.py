ea5ebf71fdf8de3137d45f518ac897416cf7ad2c
EimantasBlazevicius <eima.blaz@gmail.com>
2022-01-16 11:51:21 +0200

    fixed crud.py

--- SQL alchemy topic/proper_structure/crud.py
+++ SQL alchemy topic/proper_structure/crud.py
@@ -3,47 +3,54 @@ from sqlalchemy.orm import sessionmaker
 
 Session = sessionmaker(bind=eng)
 session = Session()
+
 """ Kaip įrašyti duomenis į lentelę:
  (Crud) """
 
-new_director = Directors({'name': 'Frank', 'surname': 'Darabont', 'rating': 7})
+new_director = Directors(name='Frankas7', surname='Darabontas2', rating=9)
 session.add(new_director)
 session.commit()
 
 """ Kaip gauti duomenis iš lentelės:
  (cRud) """
-pirmas_direktorius = session.query(Directors).get(1)
+pirmas_direktorius = session.query(Directors).get(3)
 print(pirmas_direktorius.name)
+print("------------------------------")
 
 pirmas_direktorius_tokiu_vardu = session.query(Directors).filter_by(name="Frank").one()
+print(pirmas_direktorius_tokiu_vardu)
+print("------------------------------")
 visi_direktoriai = session.query(Directors).all()
-
 for direktorius in visi_direktoriai:
     print(direktorius.name, direktorius.surname)
+print("------------------------------")
 
 # Kaip ieškoti duomenų pagal sąlygą ar šabloną:
 
-search2 = session.query(Directors).filter(Directors.price > 1000)
+search2 = session.query(Directors).filter(Directors.rating > 4)
 search3 = session.query(Directors).filter(
-    Directors.price > 1000,
-    Directors.name.ilike("2%"))
+    Directors.rating > 4,
+    Directors.name.ilike("D%"))
 
 print([i for i in search2])
+print("------------------------------")
 print([i for i in search3])
 
 """ Kaip pakeisti duomenis lentelėje:
  (crUd) """
 pirmas_direktorius_2 = session.query(Directors).get(1)
-pirmas_direktorius_2.rating = 22000
+pirmas_direktorius_2.rating = 22
 session.commit()
-direktorius_neteisingu_vardu = session.query(Directors).filter_by(name="Frank").one()
-direktorius_neteisingu_vardu.name = "Eimantas"
+
+direktorius_neteisingu_vardu = session.query(Directors).filter_by(name="Eimantas").one()
+print(direktorius_neteisingu_vardu)
+direktorius_neteisingu_vardu.name = "Quentin"
 session.commit()
 
 """ Kaip ištrinti duomenis lentelėje: 
 (cruD)
 """
-atleidziamas_direktorius = session.query(Directors).filter_by(name="Eimantas").one()
+atleidziamas_direktorius = session.query(Directors).filter_by(name="Christopher").one()
 
 session.delete(atleidziamas_direktorius)
 session.commit()
--- SQL alchemy topic/proper_structure/model.py
+++ SQL alchemy topic/proper_structure/model.py
@@ -13,7 +13,10 @@ class Directors(Base):
     name = Column(String(30), nullable=False)
     surname = Column(String(30), nullable=False)
     rating = Column(Integer, nullable=False)
-    movies = relationship('Movies', back_populates='director')
+    movies = relationship('Movies', back_populates='director', cascade="all, delete")
+
+    def __repr__(self):
+        return f'{self.name}'
 
 
 class Movies(Base):
@@ -23,9 +26,12 @@ class Movies(Base):
     title = Column(String(30), nullable=False)
     year = Column(Integer, nullable=False)
     category = Column(String(30), nullable=False)
-    director_id = Column(Integer, ForeignKey('directors.director_id'), nullable=False)
+    director_id = Column(Integer, ForeignKey('directors.director_id'), nullable=False)  # if no cascade(deletion of movies) needed, change nullable to True
     rating = Column(Integer, nullable=False)
     director = relationship('Directors', back_populates='movies')
+
+    def __repr__(self):
+        return f'Title: {self.title}, belongs to the category: {self.category}'
 
 
 Base.metadata.create_all(eng)
